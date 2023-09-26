4d60e08a863619069f24309e203624a25bf61e8a
Aelzi <96818446+Aelzi@users.noreply.github.com>
2023-09-26 23:57:43 +0700

    update

--- pages/4_Predict.py
+++ pages/4_Predict.py
@@ -7,61 +7,61 @@ import joblib
 
 df=pd.read_csv("polusi_udara_jogja2020.csv")
 
+print(df.head())
 
 
 
+# # Load the trained model and user input data
+# pipe = joblib.load(open('../model.pkl', 'rb'))
+# df = joblib.load(open('../data.pkl', 'rb'))
 
-# Load the trained model and user input data
-pipe = joblib.load(open('../model.pkl', 'rb'))
-df = joblib.load(open('../data.pkl', 'rb'))
+# st.write("""
+# # Udara Jogja Prediction App
 
-st.write("""
-# Udara Jogja Prediction App
+# This app predicts the **Udara Jogja** type!
+# """)
 
-This app predicts the **Udara Jogja** type!
-""")
+# st.sidebar.header('User Input Parameters')
 
-st.sidebar.header('User Input Parameters')
+# # User input features
+# def user_input_features():
+#     ozone = st.sidebar.slider('Ozone (O3)', 0, 81, 50)
+#     carbon_monoksida = st.sidebar.slider('Carbon Monoksida (CO)', 0, 164, 50)
+#     sulfur_dioksida = st.sidebar.slider('Sulfur Dioksida (SO2)', 0, 6, 3)
+#     particulate_matter = st.sidebar.slider('Particulate Matter 10 (PM10)', 3, 60, 40)
+#     data = {'O3': ozone,
+#             'CO': carbon_monoksida,
+#             'SO2': sulfur_dioksida,
+#             'PM10': particulate_matter}
+#     features = pd.DataFrame(data, index=[0])
+#     return features
 
-# User input features
-def user_input_features():
-    ozone = st.sidebar.slider('Ozone (O3)', 0, 81, 50)
-    carbon_monoksida = st.sidebar.slider('Carbon Monoksida (CO)', 0, 164, 50)
-    sulfur_dioksida = st.sidebar.slider('Sulfur Dioksida (SO2)', 0, 6, 3)
-    particulate_matter = st.sidebar.slider('Particulate Matter 10 (PM10)', 3, 60, 40)
-    data = {'O3': ozone,
-            'CO': carbon_monoksida,
-            'SO2': sulfur_dioksida,
-            'PM10': particulate_matter}
-    features = pd.DataFrame(data, index=[0])
-    return features
+# # Function to make predictions
+# def predict_category(ozone, carbon_monoksida, sulfur_dioksida, particulate_matter):
+#     # Create a DataFrame with the user input
+#     user_input = pd.DataFrame({'O3': [ozone],
+#                             'CO': [carbon_monoksida],
+#                             'SO2': [sulfur_dioksida],
+#                             'PM10': [particulate_matter]})
 
-# Function to make predictions
-def predict_category(ozone, carbon_monoksida, sulfur_dioksida, particulate_matter):
-    # Create a DataFrame with the user input
-    user_input = pd.DataFrame({'O3': [ozone],
-                            'CO': [carbon_monoksida],
-                            'SO2': [sulfur_dioksida],
-                            'PM10': [particulate_matter]})
+#     # Use the loaded model to make predictions
+#     prediction = pipe.predict(user_input)
 
-    # Use the loaded model to make predictions
-    prediction = pipe.predict(user_input)
+#     # Map the prediction to the corresponding category
+#     category_mapping = {1: 'Moderate', 2: 'Good', 3: 'Unhealthy'}
+#     category = category_mapping.get(prediction[0], 'Unknown')
 
-    # Map the prediction to the corresponding category
-    category_mapping = {1: 'Moderate', 2: 'Good', 3: 'Unhealthy'}
-    category = category_mapping.get(prediction[0], 'Unknown')
+#     return category
 
-    return category
+# # Get user input
+# Input = user_input_features()
+# ozone = Input['O3'].values[0]
+# carbon_monoksida = Input['CO'].values[0]
+# sulfur_dioksida = Input['SO2'].values[0]
+# particulate_matter = Input['PM10'].values[0]
 
-# Get user input
-Input = user_input_features()
-ozone = Input['O3'].values[0]
-carbon_monoksida = Input['CO'].values[0]
-sulfur_dioksida = Input['SO2'].values[0]
-particulate_matter = Input['PM10'].values[0]
+# # Predict the category
+# category = predict_category(ozone, carbon_monoksida, sulfur_dioksida, particulate_matter)
 
-# Predict the category
-category = predict_category(ozone, carbon_monoksida, sulfur_dioksida, particulate_matter)
-
-# Display the predicted category
-st.write(f"Prediction: {category} Air Quality")
+# # Display the predicted category
+# st.write(f"Prediction: {category} Air Quality")
